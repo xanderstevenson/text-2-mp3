6052b3019df341509cc0bd1271c108124dc56f1d
Alexander Stevenson <27918923+xanderstevenson@users.noreply.github.com>
2022-11-30 19:15:12 -0500

    Update action-text-2-mp3.py

--- action-text-2-mp3.py
+++ action-text-2-mp3.py
@@ -63,12 +63,12 @@ class GetAudio:
                     f"\n\nMP3 file created at { sys.path[0] }/mp3s/{file_name}.mp3\n\n",
                     fg="green",
                 )
-                # Automatically play audio sample and alert user
-                pygame.mixer.init()  # initialize mixer module
-                pygame.mixer.music.load(f"{ sys.path[0] }/mp3s/{file_name}.mp3")
-                pygame.mixer.music.play()
-                print("Audio sample will play for 3 seconds\n\n")
-                time.sleep(3)
+#                 # Automatically play audio sample and alert user
+#                 pygame.mixer.init()  # initialize mixer module
+#                 pygame.mixer.music.load(f"{ sys.path[0] }/mp3s/{file_name}.mp3")
+#                 pygame.mixer.music.play()
+#                 print("Audio sample will play for 3 seconds\n\n")
+#                 time.sleep(3)
         # handle exception (exits program)
         except FileNotFoundError:
             print(
